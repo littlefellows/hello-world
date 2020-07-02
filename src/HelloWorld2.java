06c1e5621538b1a18fdbb94d04b9cfe6eefa3e95
82102 <82102@DESKTOP-I169A17>
2020-07-02 21:51:13 +0900

    Hello java22 메세지추가

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -2,6 +2,6 @@
 public class HelloWorld2 {
 	public static void main(String [] args) {
 		System.out.println("Hello Java");
-		System.out.println("Hello Java22");
+		System.out.println("Hello java22");
 	}
 }
